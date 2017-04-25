3efa29e626f10b2440128be25d12e0a5350bda4b
Kushlev <b.kushlev@gmail.com>
2017-04-25 15:40:31 +0300

    adding solutions

--- src/ex1/Functions.scala
+++ src/ex1/Functions.scala
@@ -1,35 +1,105 @@
+//Боян Кушлев F75128
+
 package ex1
+
+import scala.annotation.tailrec
 
 object Functions {
 
-  // Връща големината на масив (без да ползва data.length!!!)
-  def length(data: List[Int]) = ??? 
-
-  // Ако cond е true връща onTrue
-  def ifelse(cond: Boolean, onTrue: Int, onFalse: Int) = ???
-
-  // Проверява дали скобите в даден масив от символи са балансирани.
-  // Коректно: (a)asda(b)(v) | (((a))) | ()(()асдасд)
-  // Грешно: )() | ((д) | ((das) (d)( 
-  def balance(chars: List[Char]): Boolean = ???
-
-  def map(chars: List[Char], f: Any) =  ???
-
-  def toUpperCase(chars: List[Char]) = {
-    def upperCase(char: Char) = ???
-  
-    ???
+  def length(data: List[Int]): Int = {
+    @tailrec
+    def loop(count: Int, data: List[Int]): Int = {
+      if (data.isEmpty) count
+      else {
+        loop(count+1, data.tail)
+      }
+    }
+    loop(0, data)
   }
 
-  // Проверява дали съществува елемент отговарящ на f
-  def exists(data: List[Int], f: Any) = ???
+  def ifelse(cond: Boolean, onTrue: Int, onFalse: Int): Int = {
+    if (cond) return onTrue
+    onFalse
+  }
 
-  // Връща масив съдържащ само елементите отговарящи на f
-  def filter(data: List[Int], f: Any) = ???
 
-  // Проверява дали всички елементи отговарят на f
-  def forall(data: List[Int], f: Any) = ???
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def loop(count: Int, chars: List[Char]): Boolean = {
+      if (count < 0) return false
+      if (chars.isEmpty){
+        if (count == 0) true
+        else false
+      }
+      else if (chars.head == '(') {
+        loop(count + 1, chars.tail)
+      }
+      else if (chars.head == ')') {
+        loop(count - 1, chars.tail)
+      }
+      else
+        loop(count, chars.tail)
+    }
+    loop(0, chars)
+  }
 
-  // Връща числото от триъгълника на Паскал отговарящо на съответния ред/колона
-  def pascal(c: Int, r: Int): Int = ???
+  def map(chars: List[Char], f: (Char) => Any): List[Any] = {
+    @tailrec
+    def loop(chars: List[Char], res: List[Any]): List[Any] = {
+      if (chars.isEmpty) res
+      else{
+        loop(chars.tail, res :+ f(chars.head))
+      }
+    }
+    loop(chars, List())
+  }
+
+  def toUpperCase(chars: List[Char]): List[Char] = {
+    def upperCase(char: Char): Char = {
+      (char-32).toChar
+    }
+
+    @tailrec
+    def loop(chars: List[Char], res: List[Char]): List[Char] = {
+      if(chars.isEmpty) res
+      else
+        loop(chars.tail, res :+ upperCase(chars.head))
+    }
+    loop(chars, List())
+  }
+
+  @tailrec
+  def exists(data: List[Int], f: (Int) => Boolean): Boolean = {
+    if(data.isEmpty) false
+    else if(f(data.head)) true
+    else exists(data.tail, f)
+  }
+
+  def filter(data: List[Int], f: (Int) => Boolean): List[Int] = {
+    @tailrec
+    def loop(data: List[Int], res: List[Int]): List[Int] = {
+      if(data.isEmpty) res
+      else{
+        if(f(data.head))
+          loop(data.tail, res :+ data.head)
+        else
+          loop(data.tail, res)
+      }
+    }
+    loop(data, List())
+  }
+
+  @tailrec
+  def forall(data: List[Int], f: (Int) => Boolean): Boolean = {
+    if(data.isEmpty) true
+    else if(!f(data.head)) false
+    else forall(data.tail, f)
+
+  }
+
+  def pascal(c: Int, r: Int): Int = {
+    if(c == 1 || c == r) 1
+    else pascal(c-1, r-1) + pascal(c, r-1)
+  }
+
 }
