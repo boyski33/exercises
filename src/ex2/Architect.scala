2c11f7e782d01ae741ad1ebb24ccb7fd226c4168
Boyan Kushlev <takarabukara@gmail.com>
2017-05-30 17:08:30 +0300

    add solution to Architect class

--- src/ex2/Architect.scala
+++ src/ex2/Architect.scala
@@ -18,10 +18,31 @@ class Architect {
    *     val o: Option[Int] = Some(6)
    *     val n: Int = o.get
    */
-  def max(xs: List[Int]): Option[Int] = ???
+  def max(xs: List[Int]): Option[Int] = {
+    def loop(xs: List[Int], m: Int): Option[Int] = {
+      if(xs.isEmpty) Some(m)
+      else {
+        if(xs.head > m) loop(xs.tail, xs.head)
+        else loop(xs.tail, m)
+      }
+    }
+
+    if(xs.isEmpty) None
+    else loop(xs, Int.MinValue)
+  }
 
   // Determines the type of given triangle: "rectangular", "equilateral", "isosceles", "random"
-  def triangleType(t: Triangle): String = ???
+  def triangleType(t: Triangle): String = {
+    if (t.a == t.b || t.b == t.c || t.a == t.c) "isosceles"
+    else if (t.a == t.b && t.b == t.c && t.a == t.c) "equilateral"
+    else {
+      val a2 = t.a * t.a
+      val b2 = t.b * t.b
+      val c2 = t.c * t.c
+      if ((a2 == b2 + c2) || (b2 == a2 + c2) || (c2 == a2 + b2)) "rectangular"
+      else "random"
+    }
+  }
 
   /*
    * Calculates the area of the provided shape, by using these formulae:
@@ -33,7 +54,23 @@ class Architect {
    *
    *  Hint: for triangles use the max function
    */
-  def area(s: Shape): Double = ???
+  def area(s: Shape): Double = s match {
+    case t: Triangle => triangleType(t) match {
+      case "rectangular" => max(List(t.a, t.b, t.c)).get match {
+        case t.a => t.b * t.c / 2.0
+        case t.b => t.c * t.a / 2.0
+        case t.c => t.a * t.b / 2.0
+      }
+      case _ => max(List(t.a, t.b, t.c)).get match {
+        case t.a => t.a * t.h / 2.0
+        case t.b => t.b * t.h / 2.0
+        case t.c => t.c * t.h / 2.0
+      }
+    }
+    case r: Rectangle => r.a * r.b
+    case t: Trapezoid => (t.a + t.b) * t.h / 2.0
+    case c: Cube => -1
+  }
 
   /*
    *  Returns the number of rectangular triangles in given list of shapes
@@ -41,7 +78,16 @@ class Architect {
    *  Hint: use the triangleType function
    */
   def findRectangulars(shapes: List[Shape]): Int = {
-    def iter(shapes: List[Shape], n: Int): Int = ???
-    iter(???, ???)
+    def iter(shapes: List[Shape], n: Int): Int = {
+      if(shapes.isEmpty) n
+      else shapes.head match {
+        case t: Triangle => triangleType(t) match {
+          case "rectangular" => iter(shapes.tail, n+1)
+          case _ => iter(shapes.tail, n)
+        }
+        case s: Shape => iter(shapes.tail, n)
+      }
+    }
+    iter(shapes, 0)
   }
 }
